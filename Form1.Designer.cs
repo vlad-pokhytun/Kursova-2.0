9719466b26d4094fbea71d940d83c631444b7f33
vlad-pokhytun <45255567+vlad-pokhytun@users.noreply.github.com>
2019-06-24 11:37:53 +0300

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,181 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.textBox1.Location = new System.Drawing.Point(91, 102);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(324, 140);
+            this.textBox1.TabIndex = 0;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.textBox2.Location = new System.Drawing.Point(631, 102);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(308, 140);
+            this.textBox2.TabIndex = 1;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.textBox3.Location = new System.Drawing.Point(101, 322);
+            this.textBox3.Multiline = true;
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(314, 121);
+            this.textBox3.TabIndex = 2;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.textBox4.Location = new System.Drawing.Point(631, 322);
+            this.textBox4.Multiline = true;
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(308, 121);
+            this.textBox4.TabIndex = 3;
+            // 
+            // button1
+            // 
+            this.button1.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.button1.Location = new System.Drawing.Point(451, 142);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(159, 41);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Text to bin";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.button2.Location = new System.Drawing.Point(451, 366);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(150, 42);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "Bin to text";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button4
+            // 
+            this.button4.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.button4.Location = new System.Drawing.Point(708, 43);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(159, 44);
+            this.button4.TabIndex = 7;
+            this.button4.Text = "Save";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button6
+            // 
+            this.button6.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.button6.Location = new System.Drawing.Point(708, 269);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(159, 41);
+            this.button6.TabIndex = 9;
+            this.button6.Text = "Save";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // button3
+            // 
+            this.button3.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.button3.Location = new System.Drawing.Point(160, 43);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(178, 44);
+            this.button3.TabIndex = 10;
+            this.button3.Text = "Load";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button5
+            // 
+            this.button5.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.button5.Location = new System.Drawing.Point(160, 269);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(178, 41);
+            this.button5.TabIndex = 11;
+            this.button5.Text = "Load";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1097, 541);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button5;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            textBox2.Text = ToBinary(textBox1.Text);
+        }
+
+        static string ToBinary(string input)
+        {
+            var builder = new System.Text.StringBuilder();
+            var bytes = System.Text.Encoding.UTF8.GetBytes(input); // Convert the text to bytes using the encoding
+
+            foreach (var b in bytes)
+                builder.Append(Convert.ToString(b, 2).PadLeft(8, '0')); //Convert the byte to its binary representation
+
+            return builder.ToString();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            textBox4.Text = ToText(textBox3.Text);
+        }
+
+        static string ToText(string bytes)
+        {
+
+            int byteCount = 8;
+            var byteArray = new byte[bytes.Length / 8]; // An array for the bytes
+            for (int i = 0; i < bytes.Length / byteCount; i++)
+            {
+                var subBytes = bytes.Substring(i * byteCount, byteCount); // Get a subpart of 8 bits
+                var b = Convert.ToByte(subBytes.TrimStart('0'), 2); // Convert the subpart to a byte
+                byteArray[i] = b; // Add the byte to the array
+            }
+
+            return System.Text.Encoding.UTF8.GetString(byteArray); // Convert the array to text using the right encoding.
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            File.WriteAllText("binary.txt", string.Join(Environment.NewLine, textBox2.Text));
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            File.WriteAllText("text.txt", string.Join(Environment.NewLine, textBox4.Text));
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            StreamReader reader = new StreamReader(@"text.txt");
+            while (!reader.EndOfStream)
+            {
+                textBox1.Text = reader.ReadLine();
+            }
+
+        }
+        private void button5_Click(object sender, EventArgs e)
+        {
+            StreamReader reader = new StreamReader(@"binary.txt");
+            while (!reader.EndOfStream)
+            {
+                textBox3.Text = reader.ReadLine();
+            }
+        }
+    }
+}
